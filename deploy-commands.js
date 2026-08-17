4435f93b65a3b40f1e22b98d8c177e9e72f941e6
jamesherlock77-dotcom <jamesherlock77@gmail.com>
2026-08-17 06:50:20 +0100

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,7 +1,8 @@
 // deploy-commands.js
-// Register slash commands (includes /ping).
+// Register slash commands (includes team options with autocomplete).
 // Usage:
 // DISCORD_TOKEN=... DISCORD_CLIENT_ID=... DISCORD_GUILD_ID=... node deploy-commands.js
+
 const { REST, Routes, SlashCommandBuilder } = require('discord.js');
 const config = require('./config');
 
@@ -18,24 +19,43 @@ if (!CLIENT_ID) {
   process.exit(1);
 }
 
-// Build command list (add more as you implement)
 const commands = [
   new SlashCommandBuilder().setName('createteam').setDescription('Create a new team')
     .addStringOption(opt => opt.setName('name').setDescription('Team name').setRequired(true))
     .addStringOption(opt => opt.setName('emoji').setDescription('Single standard emoji').setRequired(true))
     .addStringOption(opt => opt.setName('colour').setDescription('Hex colour, e.g. #5865F2').setRequired(true)),
+
   new SlashCommandBuilder().setName('invite').setDescription('Invite a user to your team')
     .addUserOption(opt => opt.setName('user').setDescription('The user to invite').setRequired(true)),
+
   new SlashCommandBuilder().setName('startscrim').setDescription('Challenge another team')
-    .addStringOption(opt => opt.setName('team').setDescription('The team you want to scrim').setRequired(true)),
+    .addStringOption(opt => opt
+      .setName('team')
+      .setDescription('The team you want to scrim')
+      .setRequired(true)
+      .setAutocomplete(true)
+    ),
+
+  new SlashCommandBuilder().setName('teammembers').setDescription("List a team's members")
+    .addStringOption(opt => opt
+      .setName('team')
+      .setDescription('Team name')
+      .setRequired(true)
+      .setAutocomplete(true)
+    ),
+
   new SlashCommandBuilder().setName('leaveteam').setDescription('Leave your current team'),
+
   new SlashCommandBuilder().setName('startgiveaway').setDescription('Start a giveaway (staff)')
     .addIntegerOption(opt => opt.setName('winners').setDescription('How many winners').setRequired(true))
     .addStringOption(opt => opt.setName('prize').setDescription("What's being given away").setRequired(true))
     .addStringOption(opt => opt.setName('ends').setDescription('How long, e.g. 10m').setRequired(true))
     .addUserOption(opt => opt.setName('hosted').setDescription("Who's hosting the giveaway")),
+
   new SlashCommandBuilder().setName('syncinvites').setDescription('Rebuild the invite DB by rescanning the invite tracker channel'),
+
   new SlashCommandBuilder().setName('checkupdate').setDescription('Check for an Animal Company update manually'),
+
   // Simple test command:
   new SlashCommandBuilder().setName('ping').setDescription('Ping test'),
 ].map(c => c.toJSON());
@@ -46,7 +66,7 @@ const commands = [
     if (GUILD_ID) {
       console.log('Refreshing guild commands...');
       await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), { body: commands });
-      console.log('Successfully reloaded guild application (/) commands.');
+      console.log('Successfully reloaded guild application (/) commands for guild', GUILD_ID);
     } else {
       console.log('Refreshing global commands (may take up to an hour)...');
       await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
