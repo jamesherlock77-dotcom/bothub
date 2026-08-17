c90393cb2c4be27fd1848be5dd1d9ec651a5a39b
jamesherlock77-dotcom <jamesherlock77@gmail.com>
2026-08-17 07:21:36 +0100

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,23 +1,15 @@
-// deploy-commands.js
-// Register slash commands (includes team options with autocomplete).
-// Usage:
-// DISCORD_TOKEN=... DISCORD_CLIENT_ID=... DISCORD_GUILD_ID=... node deploy-commands.js
+// deploy-commands.js - register slash commands with autocomplete for team options
+// Usage: DISCORD_TOKEN=... DISCORD_CLIENT_ID=... DISCORD_GUILD_ID=... node deploy-commands.js
 
 const { REST, Routes, SlashCommandBuilder } = require('discord.js');
 const config = require('./config');
 
 const token = process.env[config.DISCORD_TOKEN_ENV] || process.env.DISCORD_TOKEN;
 const CLIENT_ID = process.env.DISCORD_CLIENT_ID;
-const GUILD_ID = process.env.DISCORD_GUILD_ID; // optional for quick dev registration
+const GUILD_ID = process.env.DISCORD_GUILD_ID;
 
-if (!token) {
-  console.error('DISCORD_TOKEN not set');
-  process.exit(1);
-}
-if (!CLIENT_ID) {
-  console.error('Set DISCORD_CLIENT_ID env var (bot application ID).');
-  process.exit(1);
-}
+if (!token) { console.error('DISCORD_TOKEN not set'); process.exit(1); }
+if (!CLIENT_ID) { console.error('DISCORD_CLIENT_ID not set'); process.exit(1); }
 
 const commands = [
   new SlashCommandBuilder().setName('createteam').setDescription('Create a new team')
@@ -25,38 +17,21 @@ const commands = [
     .addStringOption(opt => opt.setName('emoji').setDescription('Single standard emoji').setRequired(true))
     .addStringOption(opt => opt.setName('colour').setDescription('Hex colour, e.g. #5865F2').setRequired(true)),
 
+  new SlashCommandBuilder().setName('teammembers').setDescription("List a team's members")
+    .addStringOption(opt => opt.setName('team').setDescription('Team name').setRequired(true).setAutocomplete(true)),
+
   new SlashCommandBuilder().setName('invite').setDescription('Invite a user to your team')
     .addUserOption(opt => opt.setName('user').setDescription('The user to invite').setRequired(true)),
 
-  new SlashCommandBuilder().setName('startscrim').setDescription('Challenge another team')
-    .addStringOption(opt => opt
-      .setName('team')
-      .setDescription('The team you want to scrim')
-      .setRequired(true)
-      .setAutocomplete(true)
-    ),
-
-  new SlashCommandBuilder().setName('teammembers').setDescription("List a team's members")
-    .addStringOption(opt => opt
-      .setName('team')
-      .setDescription('Team name')
-      .setRequired(true)
-      .setAutocomplete(true)
-    ),
-
   new SlashCommandBuilder().setName('leaveteam').setDescription('Leave your current team'),
 
-  new SlashCommandBuilder().setName('startgiveaway').setDescription('Start a giveaway (staff)')
-    .addIntegerOption(opt => opt.setName('winners').setDescription('How many winners').setRequired(true))
-    .addStringOption(opt => opt.setName('prize').setDescription("What's being given away").setRequired(true))
-    .addStringOption(opt => opt.setName('ends').setDescription('How long, e.g. 10m').setRequired(true))
-    .addUserOption(opt => opt.setName('hosted').setDescription("Who's hosting the giveaway")),
+  new SlashCommandBuilder().setName('startscrim').setDescription('Challenge another team')
+    .addStringOption(opt => opt.setName('team').setDescription('The team you want to scrim').setRequired(true).setAutocomplete(true)),
 
-  new SlashCommandBuilder().setName('syncinvites').setDescription('Rebuild the invite DB by rescanning the invite tracker channel'),
+  new SlashCommandBuilder().setName('forceadd').setDescription('Staff: force add a user to a team')
+    .addStringOption(opt => opt.setName('team').setDescription('Team name').setRequired(true).setAutocomplete(true))
+    .addUserOption(opt => opt.setName('user').setDescription('User to add').setRequired(true)),
 
-  new SlashCommandBuilder().setName('checkupdate').setDescription('Check for an Animal Company update manually'),
-
-  // Simple test command:
   new SlashCommandBuilder().setName('ping').setDescription('Ping test'),
 ].map(c => c.toJSON());
 
@@ -66,11 +41,11 @@ const commands = [
     if (GUILD_ID) {
       console.log('Refreshing guild commands...');
       await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), { body: commands });
-      console.log('Successfully reloaded guild application (/) commands for guild', GUILD_ID);
+      console.log('Successfully reloaded guild (/) commands.');
     } else {
       console.log('Refreshing global commands (may take up to an hour)...');
       await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
-      console.log('Successfully reloaded global application (/) commands.');
+      console.log('Successfully reloaded global (/) commands.');
     }
   } catch (err) {
     console.error('Failed to deploy commands', err);
