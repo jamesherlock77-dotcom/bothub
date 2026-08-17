116fd0eddb9d62ef727a1f7577ea4c399eb532f1
jamesherlock77-dotcom <jamesherlock77@gmail.com>
2026-08-17 06:27:19 +0100

    Register multiple event handlers in main.js
    Added various handler registrations for client events.

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
+// main.js additions (merge into your existing main.js)
+const { registerGiveawayHandlers } = require('./giveaways');
+const { registerScrimHandlers } = require('./scrims');
+const { registerMetaHandlers } = require('./metaupdate');
+const { refreshLeaderboardMessage } = require('./leaderboard');
+const { registerInviteHandlers } = require('./invites');
+const { registerTeamHandlers } = require('./teams');
+const { registerTicketHandlers } = require('./ticket');
 
+// after client created and logged in:
+registerTicketHandlers(client);
+registerTeamHandlers(client);
+registerInviteHandlers(client);
+registerGiveawayHandlers(client);
+registerScrimHandlers(client);
+registerMetaHandlers(client);
+
+// Optional: call refreshLeaderboardMessage(client) after ready if you want to update the leaderboard image on startup.
